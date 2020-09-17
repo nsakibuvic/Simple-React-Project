aca9446521440594f1642b3ec6c6e74354e34a0d
Nazmus Sakib <nsakibuvic@gmail.com>
2020-09-17 18:30:34 -0400

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import Login from "./Submit"
+
+var isLoggedIn = false;
+
+const currentTime= new Date().getHours();
+
+function App(){
+  return (
+    <div className="container">
+      {/*Ternary Operator*/}
+      {isLoggedIn ? <h1>Hello</h1> : <Login />}
+      {/*AND Operator*/}
+      {currentTime > 12 && <h1>Why are you still working?</h1>}
+    </div>
+  );
+}
+
+export default App;
